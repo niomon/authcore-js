c2ae3826c9fff6b30aeb01b20be918401658a96c
Elton Lee <elton2048@gmail.com>
2020-04-28 15:51:23 +0800

    Provide delete user API in management client

--- src/v2/management/user.js
+++ src/v2/management/user.js
@@ -5,7 +5,16 @@ class UserAPI {
     this.authcore = authcore
   }
 
-  async listUserEvents (userId, pageToken, rowsPerPage, order, options) {
+  /**
+   * List the event logs of a user from user ID.
+   *
+   * @param {number} userId The user's ID.
+   * @param {string} pageToken The page token for the page result. If it is empty it returns the first page.
+   * @param {number} rowsPerPage The number of events returns in a page.
+   * @param {string} order The order of the result returned.
+   * @returns {object} Result including the event logs list, page token for previous and next page and number of total items.
+   */
+  async listUserEvents (userId, pageToken, rowsPerPage, order) {
     if (typeof userId !== 'number') {
       throw new Error('userId is required and has to be number format')
     }
@@ -20,6 +29,19 @@ class UserAPI {
     const url = new URL(basePath + '/audit_logs?' + params.toString(), this.authcore.baseURL)
     const resp = await this.authcore._http.get(url.toString())
     return resp.data
+  }
+
+  /**
+   * Delete a user from user ID.
+   *
+   * @param {number} userId The user's ID.
+   */
+  async deleteUser (userId) {
+    if (typeof userId !== 'number') {
+      throw new Error('userId is required and has to be number format')
+    }
+    const url = new URL(basePath + '/users/' + userId, this.authcore.baseURL)
+    await this.authcore._http.delete(url)
   }
 }
 
